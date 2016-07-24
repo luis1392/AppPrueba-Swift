038de99728adf47409c629d7c05e87fc48e64a4f
Luis Enrique Tierrafria Rodriguez <luis@Luiss-iMac.lan>
2016-07-24 15:02:26 -0500

    test swift

--- AppDePrueba/ViewController.swift
+++ AppDePrueba/ViewController.swift
@@ -10,16 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("hola mundo")
+        myLabel.text = "Ey, esto funciona"
+        
     }
+   
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func buttonPressed(sender: AnyObject) {
+        myLabel.text = "click en el boton"
+    }
 
 }
 
